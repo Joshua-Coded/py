652a2a03d75be2070be5d3591a9dd5293a122317
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-21 04:50:13 +0200

    py

---
+++ py.py
@@ -0,0 +1,20 @@
+# redoing the pascal challenge
+
+def pascal(n):
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
+
+
+print(pascal(0))
+print(pascal(1))
+print(pascal(2))
+print(pascal(3))
+print(pascal(100))
