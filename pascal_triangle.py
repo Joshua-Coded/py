1bc1e2cddf2ea3cc5c8f68584973b6c9ea56e307
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-21 04:40:52 +0200

    oop

---
+++ pascal_triangle.py
@@ -0,0 +1,22 @@
+# define the function
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
+
+
+
+print(pascal_triangle(0))
+print(pascal_triangle(1))
+print(pascal_triangle(2))
+print(pascal_triangle(3))
+print(pascal_triangle(4))
+print(pascal_triangle(5))
